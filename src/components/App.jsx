67e63193be9fe6c8013f2a8f6611570a6f288c77
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-05-10 16:26:45 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import { Layout } from './Layout/Layout';
+
+export class App extends Component {
+  render() {
+    return <Layout></Layout>;
+  }
+}
